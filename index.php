bbf03a779285396d369b4fb434115a3be2f13205
Rustam Siraev <sirius0983@mail.ru>
2021-03-08 17:28:56 +0500

    add messengers widget

--- index.php
+++ index.php
@@ -41,6 +41,12 @@ $projectsData =  $connection->query("SELECT * FROM projects");
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:400,400i,300,700" rel="stylesheet" type="text/css" />
+    <link href="assets/css/messengers_widget.css" type="text/css" rel="stylesheet" media="screen" />
+    <link href="assets/css/messengers_widget_settings.css" type="text/css" rel="stylesheet" media="screen" />
+    <script src="assets/plugins/jquery-2.1.1.min.js"></script>
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
 </head>
 
 <body>
@@ -55,13 +61,15 @@ $projectsData =  $connection->query("SELECT * FROM projects");
         <div class="contact-container container-block">
             <ul class="list-unstyled contact-list">
                 <li class="email"><i class="fa fa-envelope"></i><a
-                            href="mailto: <?= $aboutData['email'] ?>"><?= $aboutData['email'] ?></a>
-                </li>
+                            href="mailto: <?= $aboutData['email'] ?>"><?= $aboutData['email'] ?></a></li>
                 <li class="phone"><i class="fa fa-phone"></i><a
                             href="tel:<?= $aboutData['phone'] ?>"><?= $aboutData['phone'] ?></a></li>
                 <li class="website"><i class="fa fa-globe"></i><a
                             href="https://<?= $aboutData['site'] ?>"
                             target="_blank"><?= $aboutData['site'] ?></a></li>
+                <li class="github"><i class="fa fa-github"></i><a
+                            href="https://github.com/RustamSiraev"
+                            target="_blank">GitHub</a></li>
             </ul>
         </div><!--//contact-container-->
         <div class="education-container container-block">
@@ -152,6 +160,29 @@ $projectsData =  $connection->query("SELECT * FROM projects");
     </div><!--//main-body-->
 </div>
 
+<div class="ts-mw-button" id="ts-mw-id">
+    <div class="ts-mw-icon mw-animation mw-animation-2" >
+        <div class="slides"><span class="instagram-icon"><img src="assets/images/messengers/instagram.svg" alt="Instagram"></span></div>
+        <div class="slides"><span class="viber-icon"><img src="assets/images/messengers/viber.svg" alt="Viber"></span></div>
+        <div class="slides"><span class="tg-icon"><img src="assets/images/messengers/telegram.svg" alt="Telegram"></span></div>
+        <div class="slides"><span class="whatsapp-icon"><img src="assets/images/messengers/whatsapp.svg" alt="Whatsapp"></span></div>
+        <div class="slides"><span class="messenger-icon"><img src="assets/images/messengers/messenger.svg" alt="Messenger"></span></div>
+        <div class="slides"><span class="skype-icon"><img src="assets/images/messengers/skype.svg" alt="Skype"></span></div>
+        <i class="fa fa-times" aria-hidden="true"></i>
+    </div>
+    <div class="ts-mw-block mw-no-bg mw-tips tips-right">
+        <ul class="ts-mw-list">
+            <li class="ts-mw-li ts-instagram"><a href="https://www.instagram.com/sirius0983" rel="nofollow" target="_blank"><span class="instagram-icon"><img src="assets/images/messengers/instagram.svg" alt="Instagram"></span><span class="ts-link-text">Instagram</span></a></li>
+            <li class="ts-mw-li ts-viber"><a href="viber://chat?number=%2B79178076639" rel="nofollow" target="_blank"><span class="viber-icon"><img src="assets/images/messengers/viber.svg" alt="Viber"></span><span class="ts-link-text">Viber</span></a></li>
+            <li class="ts-mw-li ts-telegram"><a href="http://t.me/Rustam_Siraev" rel="nofollow" target="_blank"><span class="tg-icon"><img src="assets/images/messengers/telegram.svg" alt="Telegram"></span><span class="ts-link-text">Telegram</span></a></li>
+            <li class="ts-mw-li ts-whatsapp"><a href="https://api.whatsapp.com/send?phone=79178076639" rel="nofollow" target="_blank"><span class="whatsapp-icon"><img src="assets/images/messengers/whatsapp.svg" alt="Whatsapp"></span><span class="ts-link-text">Whatsapp</span></a></li>
+            <li class="ts-mw-li ts-messenger"><a href="https://m.me/messenger" rel="nofollow" target="_blank"><span class="messenger-icon"><img src="assets/images/messengers/messenger.svg" alt="Messenger"></span><span class="ts-link-text">Messenger</span></a></li>
+            <li class="ts-mw-li ts-skype"><a href="skype:sirius_0983?chat" rel="nofollow" target="_blank"><span class="skype-icon"><img src="assets/images/messengers/skype.svg" alt="Skype"></span><span class="ts-link-text">Skype</span></a></li>
+        </ul>
+    </div>
+</div>
+<div class="ts-mw-substrate"></div>
+
 <footer class="footer">
     <div class="text-center">
         <small class="copyright">Designed with <i class="fa fa-heart"></i> by Rustam Siraev</small>
@@ -162,6 +193,28 @@ $projectsData =  $connection->query("SELECT * FROM projects");
 <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
 <!-- custom js -->
 <script type="text/javascript" src="assets/js/main.js"></script>
+<script>
+    $('.ts-mw-icon').click(function(){
+        $('#ts-mw-id').toggleClass('open');
+        $('.ts-mw-substrate').toggleClass('active');});
+    $('.ts-mw-li a').click(function(){
+        $('#ts-mw-id').removeClass('open');
+        $('.ts-mw-substrate').removeClass('active');});
+    $('.ts-mw-substrate').click(function(){
+        $('#ts-mw-id').removeClass('open');
+        $('.ts-mw-substrate').removeClass('active');
+    });
+    $(document).ready(function() {
+        let count = -1;
+        function NextItem() {
+            let boxes = $('.ts-mw-icon div');
+            let boxLength = boxes.length - 1;
+            count < boxLength ? count++ : count=0;
+            boxes.removeClass('active').eq(count).addClass('active');
+        }
+        setInterval(NextItem, 2000);
+    });
+</script>
 </body>
 </html>
 
